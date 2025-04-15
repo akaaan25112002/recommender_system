363c28d4a744b7753a935e3f80deb89e7c202fa9
akaaan25112002 <khoahuynguyen2530@gmail.com>
2025-04-15 23:48:12 +0700

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -8,8 +8,8 @@ def load_processed_data():
     data = pd.read_pickle("models/processed_data.pkl")
     dictionary = corpora.Dictionary.load("models/tfidf_dictionary.dict")
     tfidf = models.TfidfModel.load("models/tfidf_model.tfidf")
-    #index = similarities.Similarity.load("models/sim_index")
-    index = similarities.Similarity.load("models/tfidf_index.index")
+    index = similarities.Similarity.load("models/sim_index")
+    #index = similarities.Similarity.load("models/tfidf_index.index")
     return data, dictionary, tfidf, index
 
 @st.cache_data
