2de6f7d7dd1cb60d7cea2660a300cbeec2b956be
akaaan25112002 <khoahuynguyen2530@gmail.com>
2025-04-16 19:24:26 +0700

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,18 +1,19 @@
-# data_loader.py
 import pandas as pd
 from gensim import corpora, models, similarities
 import streamlit as st
+import ast
 
 @st.cache_resource
 def load_processed_data():
-    data = pd.read_pickle("models/processed_data.pkl")
+    data = pd.read_csv("Data/cleaned_products.csv")
+    data['tokens'] = data['tokens'].apply(ast.literal_eval)
+
     dictionary = corpora.Dictionary.load("models/tfidf_dictionary.dict")
     tfidf = models.TfidfModel.load("models/tfidf_model.tfidf")
-    index = similarities.Similarity.load("models/sim_index")
-    #index = similarities.Similarity.load("models/tfidf_index.index")
+    index = similarities.Similarity.load("models/tfidf_index.index")
+
     return data, dictionary, tfidf, index
 
 @st.cache_data
 def load_ratings():
-    ratings = pd.read_csv("Data/cleaned_ratings.csv")
-    return ratings
+    return pd.read_csv("Data/cleaned_ratings.csv")
